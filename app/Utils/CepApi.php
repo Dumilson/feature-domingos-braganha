8696604422fc604496dc8df53fb90ac8aec89e9a
Domingos <domingosbreganha9@gmail.com>
2023-01-17 14:59:42 +0100

    Metodo de consulta do Cep

---
+++ app/Utils/CepApi.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+
+class CepApi {
+    public static function getCep($cep)
+    {
+        $response = Http::get('https://viacep.com.br/ws/' . $cep . '/json/');
+
+        if ($response->successful()) {
+            $tmp = $response->json();
+
+            if ((empty($tmp['uf']) && empty($tmp['localidade'])) ||
+                (strlen($tmp['uf']) < 2 && strlen($tmp['localidade']) < 2)
+            ) {
+                return "O CEP " . $cep . " não existe!";
+            }
+
+            return $tmp;
+        }
+
+        return 'Incapaz de comunicar com o computador dos correios. Serviço temporariamente indisponível!';
+    }
+}
