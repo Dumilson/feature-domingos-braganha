00e922a2207a13723400f53aa9ad6e2b992beaf6
Domingos <domingosbreganha9@gmail.com>
2023-01-17 14:22:19 +0100

    Tabela para listar os registros

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,39 @@
 @extends('template.app')
 @section('content')
-    <h1>Teste</h1>
+<div class="mt-4">
+    <h1>
+        Cadastro de Pessoas
+    </h1>
+
+    <button class="btn btn-primary mt-4 mb-4">
+        Cadastrar Pessoa
+    </button>
+</div>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Cep</th>
+        <th scope="col">Endereço</th>
+        <th scope="col">Complemento</th>
+        <th scope="col">Bairro</th>
+        <th scope="col">Acção</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>
+            <a href="#" class="btn btn-sm btn-warning">Editar</a>
+            <a href="#" class="btn btn-sm btn-danger">Apagar</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 @endsection
